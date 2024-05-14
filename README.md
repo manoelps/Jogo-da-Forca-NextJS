b10b7029271ce39ed7bac03e4380b90aa14e42a1
Manoel Pereira dos Santos <manoelps@live.com>
2024-05-14 10:13:18 -0300

    criado a estrutura inicial do projeto

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Jogo-da-Forca-NextJS
-Jogo da forca desenvolvido com Next.JS v14.2.3
+
+Jogo da Forca, utilizando [Next.js](https://nextjs.org/)
+
+## Getting Started
+
+Primeiro, execute o servidor de desenvolvimento:
+
+```bash
+npm run dev
+# ou
+yarn dev
+# ou
+pnpm dev
+# ou
+bun dev
+```
+
+Abra [http://localhost:3000](http://localhost:3000) com seu navegador para ver o resultado.
+
+##
---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Jogo da Forca',
+  description: 'Jogo da Forca'
+};
+
+<link rel="icon" href="/favicon.ico" sizes="48x48" />;
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
