646f31a5ef2bfe4691f2eb81d26ee9c1a427ce24
Manoel Pereira dos Santos <manoelps@live.com>
2024-05-15 09:26:44 -0300

    feito o mock dos dados

---
+++ src/mocks/carrasco.ts
@@ -0,0 +1,37 @@
+export const carrasco = [
+  {
+    id: 0,
+    image: 'image-0.svg',
+    visibility: true
+  },
+  {
+    id: 1,
+    image: 'image-1.svg',
+    visibility: true
+  },
+  {
+    id: 2,
+    image: 'image-2.svg',
+    visibility: true
+  },
+  {
+    id: 3,
+    image: 'image-3.svg',
+    visibility: true
+  },
+  {
+    id: 4,
+    image: 'image-4.svg',
+    visibility: true
+  },
+  {
+    id: 5,
+    image: 'image-5.svg',
+    visibility: true
+  },
+  {
+    id: 6,
+    image: 'image-6.svg',
+    visibility: true
+  }
+];
---
+++ src/mocks/questions.ts
@@ -0,0 +1,78 @@
+export const questions = [
+  {
+    pista: 'É uma ave doméstica',
+    resposta: 'calopsita'
+  },
+  {
+    pista: 'Animal considerado o melhor amigo do homem',
+    resposta: 'cachorro'
+  },
+  {
+    pista: 'Felino conhecido por sua agilidade e elegância',
+    resposta: 'gato'
+  },
+  {
+    pista: 'Maior animal terrestre',
+    resposta: 'elefante'
+  },
+  {
+    pista: 'Animal terrestre mais rápido da África',
+    resposta: 'guepardo'
+  },
+  {
+    pista: 'Réptil que vive na água e no solo',
+    resposta: 'cobra'
+  },
+  {
+    pista: 'Ave que não voa e é símbolo da Nova Zelândia',
+    resposta: 'kiwi'
+  },
+  {
+    pista: 'Animal conhecido por sua listra preta e branca',
+    resposta: 'zebra'
+  },
+  {
+    pista: 'Peixe simbolo da amazonia',
+    resposta: 'pirarucu'
+  },
+  {
+    pista: 'Inseto que produz mel',
+    resposta: 'abelha'
+  },
+  {
+    pista: 'Cidade famosa por sua Torre Eiffel',
+    resposta: 'paris'
+  },
+  {
+    pista: 'Elemento químico símbolo Hg',
+    resposta: 'mercurio'
+  },
+  {
+    pista: 'Rio mais extenso do mundo',
+    resposta: 'amazonas'
+  },
+  {
+    pista: 'Planeta conhecido como o planeta vermelho',
+    resposta: 'marte'
+  },
+  {
+    pista: 'Onde foi assinada a Declaração de Independência dos Estados Unidos',
+    resposta: 'filadelfia'
+  },
+  {
+    pista: 'Maior órgão do corpo humano',
+    resposta: 'pele'
+  },
+  {
+    pista: 'Segundo elemento mais abundante na crosta terrestre',
+    resposta: 'silicio'
+  },
+  {
+    pista: 'Lugar onde os gladiadores lutavam na Roma Antiga',
+    resposta: 'coliseu'
+  },
+  {
+    pista: 'Cidade sede do governo do Reino Unido',
+    resposta: 'londres'
+  }
+];
