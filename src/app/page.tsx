7881da9acbcef10f83141a8942ed37547c883418
Manoel Pereira dos Santos <manoelps@live.com>
2024-05-16 17:03:03 -0300

    feito ajustes de responsividade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,13 +24,13 @@ const Home = () => {
             <Image
               src={`/${carrasco[score].image}`}
               alt=""
-              width={200}
+              width={130}
               height={291}
               className="lg:min-w-[298.232px] lg:min-h-[291px] md:min-w-[298.232px] md:min-h-[291px]"
               priority
             />
           </div>
-          <div className="flex flex-col items-center justify-center gap-8 pt-4 w-full">
+          <div className="flex flex-col items-center justify-center gap-4 pt-4 w-full">
             <div className="flex justify-center flex-wrap gap-2 font-bold text-2xl text-white w-full">
               {letters.map((letra, index) => (
                 <div
@@ -56,28 +56,40 @@ const Home = () => {
               Tentativas restantes: {score} / 6
             </div>
 
-            <div className="grid grid-cols-5 sm:grid-cols-6 md:grid-cols-9 lg:grid-cols-9 gap-2 justify-center">
-              {alphabetLetters.map((alphabet, index) => (
+            <div className="relative">
+              <div className="grid grid-cols-5 sm:grid-cols-6 md:grid-cols-9 lg:grid-cols-9 gap-2 justify-center">
+                {alphabetLetters.map((alphabet, index) => (
+                  <button
+                    key={index}
+                    className={classNames(
+                      `flex items-center justify-center shadow-md border font-semibold text-white w-14 h-14 bg-purple-600 rounded-md hover:scale-95`,
+                      { 'bg-slate-600 cursor-not-allowed': alphabet.disabled }
+                    )}
+                    onClick={() => {
+                      keyboard(alphabet.letter);
+                    }}
+                    disabled={alphabet.disabled}
+                  >
+                    {alphabet.letter}
+                  </button>
+                ))}
+              </div>
+              <div className="w-full flex items-center justify-end bottom-1 absolute lg:hidden md:hidden">
                 <button
-                  key={index}
+                  onClick={gameReset}
                   className={classNames(
-                    `flex items-center justify-center shadow-md border font-semibold text-white w-14 h-14 bg-purple-600 rounded-md hover:scale-95`,
-                    { 'bg-slate-600 cursor-not-allowed': alphabet.disabled }
+                    'px-8 py-3 lg:px-6 lg:py-2  md:px-6 md:py-2 bg-[#56BAEC] text-white font-semibold rounded-md shadow hover:scale-95'
                   )}
-                  onClick={() => {
-                    keyboard(alphabet.letter);
-                  }}
-                  disabled={alphabet.disabled}
                 >
-                  {alphabet.letter}
+                  PRÓXIMO DESAFIO
                 </button>
-              ))}
+              </div>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="w-full flex items-center justify-center py-8 gap-3">
+      <div className="w-full hidden lg:flex md:flex items-center justify-center py-8 gap-3">
         <button
           onClick={gameReset}
           className={classNames(
