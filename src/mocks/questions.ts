71eff70c1541a6263444340b5ba593b9b190e0ed
Manoel Pereira dos Santos <manoelps@live.com>
2024-05-27 10:41:31 -0300

    atualizado lista de perguntas

--- src/mocks/questions.ts
+++ src/mocks/questions.ts
@@ -1,78 +1,388 @@
 export const questions = [
   {
-    pista: 'É uma ave doméstica',
-    resposta: 'calopsita'
-  },
-  {
-    pista: 'Animal considerado o melhor amigo do homem',
-    resposta: 'cachorro'
-  },
-  {
-    pista: 'Felino conhecido por sua agilidade e elegância',
-    resposta: 'gato'
-  },
-  {
-    pista: 'Maior animal terrestre',
+    pista: 'É o maior mamífero terrestre',
     resposta: 'elefante'
   },
   {
-    pista: 'Animal terrestre mais rápido da África',
-    resposta: 'guepardo'
-  },
-  {
-    pista: 'Réptil que vive na água e no solo',
-    resposta: 'cobra'
-  },
-  {
-    pista: 'Ave que não voa e é símbolo da Nova Zelândia',
-    resposta: 'kiwi'
-  },
-  {
-    pista: 'Animal conhecido por sua listra preta e branca',
+    pista: 'É um animal conhecido por suas listras pretas e brancas',
     resposta: 'zebra'
   },
   {
-    pista: 'Peixe simbolo da amazonia',
-    resposta: 'pirarucu'
+    pista: 'É o animal terrestre mais rápido',
+    resposta: 'guepardo'
   },
   {
-    pista: 'Inseto que produz mel',
-    resposta: 'abelha'
+    pista: 'É um marsupial que vive na Austrália',
+    resposta: 'canguru'
   },
   {
-    pista: 'Cidade famosa por sua Torre Eiffel',
-    resposta: 'paris'
+    pista: 'É um peixe que pode inflar seu corpo',
+    resposta: 'baiacu'
   },
   {
-    pista: 'Elemento químico símbolo Hg',
-    resposta: 'mercurio'
+    pista: 'É uma ave conhecida por seu canto noturno',
+    resposta: 'coruja'
   },
   {
-    pista: 'Rio mais extenso do mundo',
-    resposta: 'amazonas'
+    pista: 'É um mamífero aquático que usa ecolocalização',
+    resposta: 'golfinho'
   },
   {
-    pista: 'Planeta conhecido como o planeta vermelho',
+    pista: 'É um pequeno roedor conhecido por armazenar comida',
+    resposta: 'hamster'
+  },
+  {
+    pista: 'É uma ave símbolo da paz',
+    resposta: 'pomba'
+  },
+  {
+    pista: 'É um réptil conhecido por sua carapaça',
+    resposta: 'tartaruga'
+  },
+  {
+    pista: 'É um inseto que se transforma em borboleta',
+    resposta: 'lagarta'
+  },
+  {
+    pista: 'É um grande primata que vive nas florestas tropicais',
+    resposta: 'gorila'
+  },
+  {
+    pista: 'É um mamífero que voa e se orienta pelo eco',
+    resposta: 'morcego'
+  },
+  {
+    pista: 'É um animal conhecido por sua habilidade de construir represas',
+    resposta: 'castor'
+  },
+  {
+    pista: 'É uma ave que não voa e vive na Antártica',
+    resposta: 'pinguim'
+  },
+  {
+    pista: 'É um mamífero marinho com dentes grandes e afiados',
+    resposta: 'tubarao'
+  },
+  {
+    pista: 'É um animal conhecido por sua habilidade de mudar de cor',
+    resposta: 'camaleao'
+  },
+  {
+    pista: 'É um grande herbívoro africano com chifre no nariz',
+    resposta: 'rinoceronte'
+  },
+  {
+    pista: 'É uma planta que cresce em desertos',
+    resposta: 'cacto'
+  },
+  {
+    pista: 'É a maior floresta tropical do mundo',
+    resposta: 'Amazonia'
+  },
+  {
+    pista: 'É o maior mamífero marinho',
+    resposta: 'baleia'
+  },
+  {
+    pista: 'É o bioma caracterizado por invernos longos e frios',
+    resposta: 'tundra'
+  },
+  {
+    pista: 'É um corpo celeste que orbita um planeta',
+    resposta: 'lua'
+  },
+  {
+    pista: 'É o fenômeno natural de luzes no céu polar',
+    resposta: 'aurora'
+  },
+
+  {
+    pista: 'É o planeta mais próximo do Sol',
+    resposta: 'jercurio'
+  },
+  {
+    pista: 'É o maior planeta do sistema solar',
+    resposta: 'jupiter'
+  },
+  {
+    pista: 'É a estrela mais próxima da Terra',
+    resposta: 'sol'
+  },
+  {
+    pista: 'É o planeta conhecido como o Planeta Vermelho',
     resposta: 'marte'
   },
   {
-    pista: 'Onde foi assinada a Declaração de Independência dos Estados Unidos',
-    resposta: 'filadelfia'
+    pista: 'É o planeta com anéis mais visíveis',
+    resposta: 'saturno'
   },
   {
-    pista: 'Maior órgão do corpo humano',
-    resposta: 'pele'
+    pista: 'É o menor planeta do sistema solar',
+    resposta: 'mercurio'
   },
   {
-    pista: 'Segundo elemento mais abundante na crosta terrestre',
-    resposta: 'silicio'
+    pista: 'É a ciência que estuda os corpos celestes',
+    resposta: 'astronomia'
   },
   {
-    pista: 'Lugar onde os gladiadores lutavam na Roma Antiga',
-    resposta: 'coliseu'
+    pista: 'É um corpo celeste que orbita uma estrela',
+    resposta: 'planeta'
   },
   {
-    pista: 'Cidade sede do governo do Reino Unido',
-    resposta: 'londres'
+    pista: 'É um planeta anão localizado no cinturão de asteroides',
+    resposta: 'ceres'
+  },
+  {
+    pista: 'É o planeta mais distante do Sol',
+    resposta: 'netuno'
+  },
+  {
+    pista: 'É o fenômeno de alinhamento da Terra, Lua e Sol',
+    resposta: 'eclipse'
+  },
+  {
+    pista: 'É o planeta mais semelhante à Terra em tamanho e composição',
+    resposta: 'venus'
+  },
+  {
+    pista: 'É uma grande nuvem de gás e poeira no espaço',
+    resposta: 'nebulosa'
+  },
+  {
+    pista: 'É o nome dado ao colapso de uma estrela massiva',
+    resposta: 'supernova'
+  },
+  {
+    pista: 'É um pequeno corpo rochoso que orbita o Sol',
+    resposta: 'asteroide'
+  },
+  {
+    pista: 'É a fase inicial da vida de uma estrela',
+    resposta: 'protostar'
+  },
+  {
+    pista: 'É a capital da Itália',
+    resposta: 'roma'
+  },
+  {
+    pista: 'É a moeda usada no Japão',
+    resposta: 'iene'
+  },
+  {
+    pista: 'É a montanha mais alta do mundo',
+    resposta: 'everest'
+  },
+  {
+    pista: "É o autor de 'Dom Quixote'",
+    resposta: 'cervantes'
+  },
+  {
+    pista: 'É o maior país da América do Sul',
+    resposta: 'brasil'
+  },
+  {
+    pista: 'É um jogo de tabuleiro com 64 casas',
+    resposta: 'xadrez'
+  },
+  {
+    pista: 'É um dos sete pecados capitais',
+    resposta: 'gula'
+  },
+  {
+    pista: 'É um elemento químico com símbolo O',
+    resposta: 'oxigenio'
+  },
+  {
+    pista: 'É a rede social fundada por Mark Zuckerberg',
+    resposta: 'facebook'
+  },
+  {
+    pista: 'É um tipo de tecido muscular do coração',
+    resposta: 'miocardio'
+  },
+  {
+    pista: 'É um instrumento usado para medir temperatura',
+    resposta: 'termometro'
+  },
+  {
+    pista: "É o autor de '1984'",
+    resposta: 'orwell'
+  },
+  {
+    pista: 'É o ponto mais alto de um triângulo',
+    resposta: 'vertice'
+  },
+  {
+    pista: 'É o país onde se encontra o Taj Mahal',
+    resposta: 'india'
+  },
+  {
+    pista: 'É uma arte marcial originária do Japão',
+    resposta: 'judo'
+  },
+  {
+    pista: 'É a capital do Canadá',
+    resposta: 'ottawa'
+  },
+  {
+    pista: 'É um símbolo matemático de adição',
+    resposta: 'mais'
+  },
+  {
+    pista: 'É um instrumento musical de teclas',
+    resposta: 'piano'
+  },
+  {
+    pista: 'É o esporte em que se usa uma raquete e uma bolinha amarela',
+    resposta: 'tenis'
+  },
+  {
+    pista: 'É a fórmula química da água',
+    resposta: 'h2o'
+  },
+  {
+    pista: 'É a capital da Alemanha',
+    resposta: 'berlim'
+  },
+  {
+    pista: 'É a maior ilha do mundo',
+    resposta: 'groenlandia'
+  },
+  {
+    pista: 'É o criador do personagem Mickey Mouse',
+    resposta: 'disney'
+  },
+  {
+    pista: 'É o elemento químico com símbolo H',
+    resposta: 'hidrogenio'
+  },
+  {
+    pista: 'É a deusa do amor na mitologia grega',
+    resposta: 'afrodite'
+  },
+  {
+    pista: 'É a capital da Austrália',
+    resposta: 'camberra'
+  },
+  {
+    pista: 'É a peça mais importante no xadrez',
+    resposta: 'rei'
+  },
+  {
+    pista: "É o autor de 'Hamlet'",
+    resposta: 'shakespeare'
+  },
+  {
+    pista: 'É a moeda usada nos Estados Unidos',
+    resposta: 'dolar'
+  },
+  {
+    pista: 'É a ciência que estuda os seres vivos',
+    resposta: 'biologia'
+  },
+  {
+    pista: 'É o líquido que sai de uma planta cortada',
+    resposta: 'seiva'
+  },
+  {
+    pista: 'É um animal conhecido por sua habilidade de construir teias',
+    resposta: 'aranha'
+  },
+  {
+    pista: 'É a maior estrela conhecida',
+    resposta: 'betelgeuse'
+  },
+  {
+    pista: 'É a forma geométrica com quatro lados iguais',
+    resposta: 'quadrado'
+  },
+  {
+    pista: 'É a capital do Egito',
+    resposta: 'cairo'
+  },
+  {
+    pista: 'É a ciência que estuda os processos mentais e o comportamento',
+    resposta: 'psicologia'
+  },
+  {
+    pista: 'É a famosa torre inclinada na Itália',
+    resposta: 'pisa'
+  },
+  {
+    pista: 'É o metal mais leve',
+    resposta: 'litio'
+  },
+  {
+    pista: 'É o tecido do corpo humano que transporta oxigênio',
+    resposta: 'sangue'
+  },
+  {
+    pista: 'É o pigmento responsável pela cor verde nas plantas',
+    resposta: 'clorofila'
+  },
+  {
+    pista: 'É a principal molécula que armazena energia nas células',
+    resposta: 'atp'
+  },
+  {
+    pista: 'É o processo pelo qual as plantas produzem seu alimento',
+    resposta: 'fotossintese'
+  },
+  {
+    pista: 'É a unidade básica da vida',
+    resposta: 'celula'
+  },
+  {
+    pista: 'É a parte da planta responsável pela absorção de água e nutrientes',
+    resposta: 'raiz'
+  },
+  {
+    pista: 'É o nome dado ao tecido condutor de seiva nas plantas',
+    resposta: 'xilema'
+  },
+  {
+    pista:
+      'É o processo de divisão celular que resulta em duas células idênticas',
+    resposta: 'mitose'
+  },
+  {
+    pista: 'É o grupo de plantas que não produzem flores',
+    resposta: 'gimnospermas'
+  },
+  {
+    pista: 'É o cientista conhecido como o pai da genética',
+    resposta: 'mendel'
+  },
+  {
+    pista: 'É a estrutura que protege a semente nas plantas angiospermas',
+    resposta: 'fruto'
+  },
+  {
+    pista: 'É o nome dado ao organismo que vive à custa de outro',
+    resposta: 'parasita'
+  },
+  {
+    pista: 'É a molécula que carrega a informação genética',
+    resposta: 'dna'
+  },
+  {
+    pista: 'É a fase de crescimento de uma planta jovem',
+    resposta: 'plantula'
+  },
+  {
+    pista: 'É a classificação científica que agrupa espécies semelhantes',
+    resposta: 'genero'
+  },
+  {
+    pista: 'É o tipo de reprodução que envolve apenas um progenitor',
+    resposta: 'assexuada'
+  },
+  {
+    pista: 'É o órgão onde ocorre a fotossíntese',
+    resposta: 'folha'
+  },
+  {
+    pista: 'É a ciência que estuda os fungos',
+    resposta: 'micologia'
   }
 ];
